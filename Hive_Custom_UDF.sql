405e023833deb1b0b7744f26b769abcbfe80022c
Sandeep Kunkunuru <sandeep.kunkunuru@gmail.com>
2014-08-18 08:41:59 -0400

    Filtering null and blank values from udf results using hive ql filters

--- Hive_Custom_UDF.sql
+++ Hive_Custom_UDF.sql
@@ -11,6 +11,12 @@ SELECT split(entry, ":")[0],split(entry, ":")[1], split(entry, ":")[2],
 split(entry, ":")[3],split(entry, ":")[4], split(entry, ":")[5]
 FROM strange_string LATERAL VIEW explode(rowWithHeader(split(strange, "\\|"), 0)) entryTable AS entry;
 
+SELECT split(t1.entry, ":")[0],split(t1.entry, ":")[1], split(t1.entry, ":")[2],
+  split(t1.entry, ":")[3],split(t1.entry, ":")[4], split(t1.entry, ":")[5]
+FROM  (select explode(rowWithHeader(split(strange, "\\|"), 0)) as entry from strange_string) as t1
+where t1.entry is not null and t1.entry <> '';
+
+
 -- NULL	NULL	NULL	NULL	NULL	NULL
 -- a	d	e	z	y	q
 -- a	d	e	1	s	p
