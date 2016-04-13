2d4560abd537ad2591ecba85bb7899c0fc70e597
Sandeep Kunkunuru <sandeep.kunkunuru@visualiq.com>
2016-04-13 11:52:38 -0400

    Hbase: sample commands

--- shell/hbase.sh
+++ shell/hbase.sh
@@ -10,12 +10,6 @@ hdfs dfs -chmod g+w /user/root
 
 rm  sample_commands.txt
 
-# hbase-mr
-pushd $PROJECT_HOME/hbase-mr/
-mvn clean assembly:assembly install
-popd
-
-
 echo "
     create 'edges', 'neighbors'
     list 'edges'
@@ -24,10 +18,12 @@ echo "
 
 hbase shell sample_commands.txt
 
-hdfs dfs -mkdir cc_edge
-hdfs dfs -put $PEGASUS_HOME/catepillar_star.edge cc_edge
+# hdfs dfs -mkdir cc_edge
+# hdfs dfs -put $PEGASUS_HOME/catepillar_star.edge cc_edge
 
-hadoop jar $PROJECT_HOME/hbase-mr/target/hbase-mr-0.1-jar-with-dependencies.jar cc_edge edges 11 3 makesym
+# hadoop jar $PROJECT_HOME/hbase-mr/target/hbase-mr-0.1-jar-with-dependencies.jar cc_edge edges 11 3 makesym
+
+rm  sample_commands.txt
 
 echo "
     disable 'edges'
@@ -37,28 +33,30 @@ echo "
 
 hbase shell sample_commands.txt
 
-#
-#touch  sample_commands.txt
-#echo "
-#    create 'test', 'cf'
-#    list 'test'
-#    put 'test', 'row1', 'cf:a', 'value1'
-#    put 'test', 'row2', 'cf:b', 'value2'
-#    put 'test', 'row3', 'cf:c', 'value3'
-#    put 'test', 'row4', 'cf:d', 'value4'
-#    scan 'test'
-#    get 'test', 'row1'
-#    exit
-#    " >> sample_commands.txt
-#
-#hbase shell sample_commands.txt
-#
-#hadoop jar ${HBASE_HOME}/lib/hbase-server-1.2.0.jar rowcounter test
-#
-#echo "
-#    disable 'test'
-#    drop 'test'
-#    exit
-#    " > sample_commands.txt
-#
-#hbase shell sample_commands.txt
+rm  sample_commands.txt
+
+echo "
+    create 'test', 'cf'
+    list 'test'
+    put 'test', 'row1', 'cf:a', 'value1'
+    put 'test', 'row2', 'cf:b', 'value2'
+    put 'test', 'row3', 'cf:c', 'value3'
+    put 'test', 'row4', 'cf:d', 'value4'
+    scan 'test'
+    get 'test', 'row1'
+    exit
+    " >> sample_commands.txt
+
+hbase shell sample_commands.txt
+
+hadoop jar ${HBASE_HOME}/lib/hbase-server-1.2.0.jar rowcounter test
+
+echo "
+    disable 'test'
+    drop 'test'
+    exit
+    " > sample_commands.txt
+
+hbase shell sample_commands.txt
+
+rm sample_commands.txt
