c3ed8785434aee0b08acec0c17e17b38ab84d579
Sandeep Kunkunuru <SKunkunuru@ad.e-dialog.com>
2016-08-28 17:54:06 -0400

    Data Gen : Benchmark 100K and 1MM : Parallel stream, Stream and array

---
+++ datagen/README.md
@@ -0,0 +1,11 @@
+100K Benchmark
+---------------
+* Parallel Stream - 87.299 sec
+* Stream - 162.542 sec
+* Array - 162.939 sec
+
+1MM Benchmark
+-------------
+* Parallel Stream - 701.22 sec
+* Stream - 1689.023 sec
+* Array - 1586.203
---
+++ datagen/src/test/java/test/me/tingri/datagen/util/RecordGeneratorTest_1MM.java
@@ -0,0 +1,60 @@
+package test.me.tingri.datagen.util;
+
+import me.tingri.big_data.util.TimeIt;
+import me.tingri.datagen.models.Record;
+import me.tingri.datagen.util.RecordGenerator;
+import org.junit.Test;
+
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by skunkunuru on 8/27/2016.
+ */
+public class RecordGeneratorTest_1MM {
+    @Test
+    public void records1() throws Exception {
+        Stream<Record> parallelStream = TimeIt.time(() -> testGenerateParallelRecordStream());
+    }
+
+    @Test
+    public void records2() throws Exception {
+        Stream<Record> recordStream = TimeIt.time(() -> testGenerateRecordStream());
+    }
+
+    @Test
+    public void records3() throws Exception {
+         Record[] records = TimeIt.time(() -> testGenerateRecords());
+    }
+
+    public Record[] testGenerateRecords() {
+        Record[] records = RecordGenerator.records("conversions,impressions,channel", ",", 1000000);
+
+        for (Record record: records){
+            assertNotNull("record" , record);
+        }
+
+        return records;
+    }
+
+    public Stream<Record> testGenerateRecordStream() {
+        Stream<Record> records = RecordGenerator.records("conversions,impressions,channel", ",", 1000000L);
+
+        records.forEach((record) -> {
+            assertNotNull("record" , record);
+        });
+
+        return records;
+    }
+
+    public Stream<Record> testGenerateParallelRecordStream() {
+        Stream<Record> records = RecordGenerator.records("conversions,impressions,channel", ",", 1000000L);
+
+        records.parallel().forEach((record) -> {
+            assertNotNull("record" , record);
+        });
+
+        return records;
+    }
+}
