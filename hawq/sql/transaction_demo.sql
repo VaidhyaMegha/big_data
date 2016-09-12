3d257b154518636a59d93db6a91e770e420aaff9
Sandeep Kunkunuru <sandeep.kunkunuru@gmail.com>
2016-09-12 09:11:33 -0400

    HAWQ Transation : Updated demo : Delete/Updated unsupported

--- hawq/sql/transaction_demo.sql
+++ hawq/sql/transaction_demo.sql
@@ -27,4 +27,20 @@ template1=# select * from transaction_test;
  123
 (1 row)
 
+template1=# begin transaction;
+BEGIN
+template1=# delete from transaction_test;
+ERROR:  Delete append-only table statement not supported yet
+template1=# select * from transaction_test;
+ERROR:  current transaction is aborted, commands ignored until end of transaction block
+template1=# end
+template1-# ;
+ROLLBACK
 template1=#
+template1=# begin transaction;
+BEGIN
+template1=# update transaction_test set col1 = '456';
+ERROR:  Update append-only table statement not supported yet
+template1=# end;
+ROLLBACK
+template1=# 
